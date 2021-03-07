f7928508de8fcd5fb4c780c041829deae59783f2
xMiCx <70638947+Mickuppom@users.noreply.github.com>
2021-03-07 16:02:32 +0700

    Update profile_screen.dart

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -15,49 +15,50 @@ class _ProfileScreenState extends State<ProfileScreen> {
         body: Column(
           children: <Widget>[
             Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [Colors.redAccent, Colors.redAccent])),
-                child: Container(
-                  width: double.infinity,
-                  height: 200.0,
-                  child: Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            "https://scontent.fbkk2-4.fna.fbcdn.net/v/t1.0-9/109045304_3346971018703270_6843698399665044772_o.jpg?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeHgoB1Eoo1qwQ7zT2uT6mBC3CPPzsoQnwLcI8_OyhCfAgsfJuNXUBLduzMCYF0c3f5pSddDAisPlxTMSnOOezsu&_nc_ohc=AV7JM_UUgcAAX_dJcg-&_nc_ht=scontent.fbkk2-4.fna&oh=d0fb18e990f55f74bb98a21105d8cfd0&oe=60686386",
-                          ),
-                          radius: 50.0,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.redAccent, Colors.redAccent])),
+              child: Container(
+                width: double.infinity,
+                height: 200.0,
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          "https://scontent.fbkk2-4.fna.fbcdn.net/v/t1.0-9/109045304_3346971018703270_6843698399665044772_o.jpg?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeHgoB1Eoo1qwQ7zT2uT6mBC3CPPzsoQnwLcI8_OyhCfAgsfJuNXUBLduzMCYF0c3f5pSddDAisPlxTMSnOOezsu&_nc_ohc=AV7JM_UUgcAAX_dJcg-&_nc_ht=scontent.fbkk2-4.fna&oh=d0fb18e990f55f74bb98a21105d8cfd0&oe=60686386",
                         ),
-                        SizedBox(
-                          height: 10.0,
+                        radius: 50.0,
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        "Pratpong Muaengwong",
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          color: Colors.white,
                         ),
-                        Text(
-                          "Pratpong Muaengwong",
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            color: Colors.white,
-                          ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        "Computer Science Student",
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.white,
                         ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Text(
-                          "Computer Science Student",
-                          style: TextStyle(
-                            fontSize: 12.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                )),
+                ),
+              ),
+            ),
             Container(
               child: Padding(
                 padding: const EdgeInsets.symmetric(
